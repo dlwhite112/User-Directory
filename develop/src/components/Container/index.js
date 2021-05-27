886148378e5cf115769806cbbcc3a0f9eab13190
D.L. White - Vunte Ron <dlwhite112@gmail.com>
2021-05-27 18:40:31 -0500

    filter searchbar funct added

--- develop/src/components/Container/index.js
+++ develop/src/components/Container/index.js
@@ -1,22 +1,11 @@
 import React, { Component } from "react";
-import SeachBox from "../SearchBox";
 import Table from "../Table";
 
-
 export default class Container extends Component {
-  state = {
-    search: "",
-  };
-
-  handleInputChange = (event) => {
-    this.setState({ search: event.target.value });
-  };
-
   render() {
     return (
       <div>
-        <SeachBox />
-        <Table/>
+        <Table />
       </div>
     );
   }
--- develop/src/components/Table/index.js
+++ develop/src/components/Table/index.js
@@ -1,37 +1,92 @@
 import React, { Component } from "react";
 import TableData from "../TableData";
 import API from "../../utils/API";
+import SeachBox from "../../components/SearchBox/index";
 
 export default class Table extends Component {
   state = {
     employees: [],
+    search: "",
   };
 
   componentDidMount() {
     this.employeeResults();
   }
 
+  handleInputChange = (event) => {
+    this.setState({ search: event.target.value.toLowerCase() });
+  };
+
   employeeResults = () => {
     API.getEmployees()
       .then((res) => this.setState({ employees: res.data.results }))
       .catch((err) => console.log(err));
   };
+  filteredRes = () =>
+    this.state.employees.filter((person) => {
+      return person.name.first.toLowerCase().includes(this.state.search);
+    });
+
+  nameSort = () => {
+    if (this.state) {
+      this.setState(
+        this.state.employees.sort((a, b) =>
+          a.name.first.localeCompare(b.name.first)
+        )
+      );
+    }
+  };
+  phoneSort = () => {
+    if (this.state) {
+      this.setState(
+        this.state.employees.sort((a, b) => a.phone.localeCompare(b.phone))
+      );
+    }
+  };
+  emailSort = () => {
+    if (this.state) {
+      this.setState(
+        this.state.employees.sort((a, b) => a.email.localeCompare(b.email))
+      );
+    }
+  };
+  dobSort = () => {
+    if (this.state) {
+      this.setState(
+        this.state.employees.sort((a, b) =>
+          a.dob.date.localeCompare(b.dob.date)
+        )
+      );
+    }
+  };
 
   render() {
     return (
       <div>
+        <SeachBox
+          value={this.state.search}
+          handleInputChange={this.handleInputChange}
+        />
         <table>
           <thead>
             <tr>
-              <th>Image</th>
-              <th>Name</th>
-              <th>Phone</th>
-              <th>Email</th>
-              <th>DOB</th>
+              <th></th>
+              <th>
+                <button onClick={this.nameSort}>Name 🔼</button>
+              </th>
+              <th>
+                <button onClick={this.phoneSort}>Phone 🔼</button>
+              </th>
+              <th>
+                <button onClick={this.emailSort}>Email 🔼</button>
+              </th>
+              <th>
+                <button onClick={this.dobSort}>DOB 🔼</button>
+              </th>
             </tr>
           </thead>
           <tbody>
-            <TableData employees={this.state.employees} />
+            <TableData employees={this.filteredRes()} />
           </tbody>
         </table>
       </div>
